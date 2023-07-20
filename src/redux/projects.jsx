43e7414cb9abea8002c83e4989ba60930dcd1d40
franclobo <fjbl2788@gmail.com>
2023-07-20 16:32:27 -0500

    revevrt style changes

--- src/redux/projects.jsx
+++ src/redux/projects.jsx
@@ -1,4 +1,4 @@
-const url = 'https://potfolio-backend.onrender.com';
+const url = 'http://127.0.0.1:3000/';
 const GETPROJECTS = 'redux/projects/GETPROJECTS';
 
 function projectReducer(state = [], action) {
